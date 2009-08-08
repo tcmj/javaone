f7996f89c1141665b050e7254ebeae080cc05745
tomdeu <tomdeu@307e33b6-52ee-11de-9d99-59ef8d702e28>
2009-08-08 01:32:28 +0000

    toolbox: VersionInfo class added
    git-svn-id: https://tcmj.googlecode.com/svn/trunk@44 307e33b6-52ee-11de-9d99-59ef8d702e28

---
+++ projects/common_toolbox/src/com/tcmj/common/VersionInfo.java
@@ -0,0 +1,24 @@
+/* $HeadURL$
+ * Copyright(c) 2005 tcmj.  All Rights Reserved. */
+package com.tcmj.common;
+
+/**
+ * Version Info for com.tcmj.common
+ * @author tcmj - Thomas Deutsch
+ * @version $Revision$
+ * @since $Date$
+ */
+public class VersionInfo {
+
+    public static final String getVersion() {
+        return "$Date$";
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
