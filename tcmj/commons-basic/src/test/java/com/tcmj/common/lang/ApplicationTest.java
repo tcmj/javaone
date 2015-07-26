dfe4375f991ee71f6367e3e3c26e95f231a3e0c5
tcmj <tomdeu@gmail.com>
2015-07-26 23:47:45 +0200

    MemoryUtils and HummanReadable and Application

--- tcmj/commons-basic/src/test/java/com/tcmj/common/lang/ApplicationTest.java
+++ tcmj/commons-basic/src/test/java/com/tcmj/common/lang/ApplicationTest.java
@@ -1,74 +1,106 @@
 package com.tcmj.common.lang;
 
-import com.tcmj.common.lang.Application;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- *
- * @author Thomas Deutsch <thomas-deutsch(a.t)tcmj.de>
+ * Application
  */
 public class ApplicationTest {
 
-    public ApplicationTest() {
-    }
-
+    /** slf4j Logging framework. */
+    private static final transient Logger LOG = LoggerFactory.getLogger(ApplicationTest.class);
 
     @BeforeClass
     public static void setUpClass() throws Exception {
+        System.getProperties().entrySet().forEach(entry -> {
+            System.out.println("key=" + entry.getKey() + " \t = " + entry.getValue());
+        });
     }
 
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
+    public ApplicationTest() {
     }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
 
     /**
      * Test of getApplicationTitle method, of class Application.
      */
     @Test
     public void testGetApplicationTitle() {
-        System.out.println("getApplicationTitle");
         Class context = getClass();
-        String result = Application.getApplicationTitle(context);
+        String result = Application.get(context).getApplicationTitle();
+        LOG.info("getApplicationTitle: '{}'", result);
         assertNotNull(result);
     }
-
 
     /**
      * Test of getApplicationVersion method, of class Application.
      */
     @Test
     public void testGetApplicationVersion() {
-        System.out.println("getApplicationVersion");
         Class context = getClass();
-        String result = Application.getApplicationVersion(context);
+        String result = Application.get(context).getApplicationVersion();
+        LOG.info("getApplicationVersion: '{}'", result);
         assertNotNull(result);
     }
-
 
     /**
      * Test of getApplicationVendor method, of class Application.
      */
     @Test
     public void testGetApplicationVendor() {
-        System.out.println("getApplicationVendor");
         Class context = getClass();
-        String result = Application.getApplicationVendor(context);
+        String result = Application.get(context).getApplicationVendor();
+        LOG.info("getApplicationVendor: '{}'", result);
         assertNotNull(result);
     }
 
+    /**
+     * <p>
+     * Test of getJavaVersionString method, of class Application.
+     */
+    @Test
+    public void testGetSystemStrings() {
+        String result = Application.get(getClass()).getJavaVersionString();
+        LOG.info("getJavaVersionString: '{}'", result);
+        assertNotNull(result);
+
+        result = Application.get(getClass()).getJavaEncodingInfos();
+        LOG.info("getJavaEncodingInfos: '{}'", result);
+        assertNotNull(result);
+
+        result = Application.get(getClass()).getJavaTimezone();
+        LOG.info("getJavaTimezone: '{}'", result);
+        assertNotNull(result);
+
+        result = Application.get(getClass()).getJavaVmName();
+        LOG.info("getJavaVmName: '{}'", result);
+        assertNotNull(result);
+
+        result = Application.get(getClass()).getOsName();
+        LOG.info("getOsName: '{}'", result);
+        assertNotNull(result);
+
+    }
+
+    /**
+     * Test of getJavaClassVersion method, of class Application.
+     */
+    @Test
+    public void testGetJavaClassVersion() {
+        Float result = Application.get(getClass()).getJavaClassVersion();
+        LOG.info("getJavaClassVersion: '{}'", result);
+        assertNotNull(result);
+    }
+/**
+     * Test of getMaxMemory method, of class Application.
+     */
+    @Test
+    public void testGetMaxMemory() {
+        String result = Application.get(getClass()).getMaxMemory();
+        LOG.info("getMaxMemory: '{}'", result);
+        assertNotNull(result);
+    }
 }
